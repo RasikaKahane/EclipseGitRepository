5bb7d17366d93f9643f39d18d4c589e225de5de6
Rasika <rasikashedage@yahoo.com>
2026-04-22 11:27:12 +0530

    Com.jen

--- JenkinsEclipse/src/com/javaProjDemo.java
+++ JenkinsEclipse/src/com/javaProjDemo.java
@@ -4,8 +4,15 @@ public class javaProjDemo {
 
 	public static void main(String[] args) {
 
-
+		javaProjDemo pj=new javaProjDemo();
+		pj.mi();
 		System.out.println("Hi Project H R u");
+		System.out.println("Java project");
+	}
+	
+	public void mi()
+	{
+		System.out.println("This is the Test mi ");
 	}
 
 }
